c1ccca33fc9f7194f823e9d58b9b8b27619934b9
Rodrigorubiks <rodrigo.gideao@gmail.com>
2024-09-17 16:49:36 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// Seleciona o botão com o ID 'cta-button'
+const ctaButton = document.getElementById('cta-button');
+
+// Adiciona um evento de Clique ao botão
+ctaButton.addEventListener('click', function() {
+    alert('Você será um super programador!');
+});
+
